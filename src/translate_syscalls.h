ee02cb5832cb6588a990a87e699f144c534fe3a9
Ryan Johnson <ryan.v.johnson3@gmail.com>
2016-09-06 13:56:20 -0400

    updated comments in translate syscalls header
    Signed-off-by: Ryan Johnson <ryan.v.johnson3@gmail.com>

--- src/translate_syscalls.h
+++ src/translate_syscalls.h
@@ -1,7 +1,7 @@
 #ifndef TRANSLATE_SYSCALLS_H
 #define TRANSLATE_STSCALLS_H
 
-char *get_proc_name(vmi_instance_t vmi, vmi_pid_t pid);			/* gets the process name */
+char *get_proc_name(vmi_instance_t vmi, vmi_pid_t pid);			/* gets the process name of the process with the pid that is input */
 void print_syscall_info(vmi_instance_t vmi, vmi_event_t *event);	/* translates and prints syscall information */
 void print_sysret_info(vmi_instance_t vmi, vmi_event_t *event);		/* translates and prints return information */
 
