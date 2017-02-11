6543dc6d15cf73505b41a09ca9c03c780570d795
W. Michael Petullo <mike@flyn.org>
2017-02-10 23:22:22 -0500

    Allow specification of system calls guestrace should instrument
    Signed-off-by: W. Michael Petullo <mike@flyn.org>

--- src/guestrace.c
+++ src/guestrace.c
@@ -10,9 +10,10 @@
 GtLoop *loop = NULL;
 
 /* Variables to hold command-line options and arguments. */
-char *name       = NULL;
-gboolean silent  = FALSE;
-gboolean verbose = FALSE;
+char *name            = NULL;
+char *instrument_list = NULL;
+gboolean silent       = FALSE;
+gboolean verbose      = FALSE;
 
 static void
 gt_close_handler (int sig)
@@ -60,7 +61,13 @@ done:
 static void
 usage()
 {
-	fprintf(stderr, "usage: guestrace [-s] [-v] -n <VM name>\n");
+	fprintf(stderr, "usage: guestrace [-i syscall1,syscall2] [-s] [-v] "
+	                "-n <VM name>\n"
+	                "\n"
+	                "-i  specify subset of system calls to instrument\n"
+	                "-s  operate in silent mode (no output on syscall/ret\n"
+	                "-v  verbose\n"
+	                "-n  name of guest to instrument\n");
 }
 
 static int
@@ -80,15 +87,105 @@ message(const char *format, ...)
 	return rc;
 }
 
+void *silent_syscall(GtGuestState *state, gt_pid_t pid, gt_tid_t tid, void *user_data)
+{
+    return NULL;
+}
+
+void silent_sysret(GtGuestState *state, gt_pid_t pid, gt_tid_t tid, void *user_data) {
+}
+
+static GtCallbackRegistry *
+registry_dup(const GtCallbackRegistry *registry)
+{
+	GtCallbackRegistry *copy;
+	int count = 0;
+
+	while (NULL != registry[count].name) {
+		count++;
+	}
+
+	copy = g_new0(GtCallbackRegistry, count + 1);
+
+	for (int i = 0; i < count; i++) {
+		copy[i] = registry[i];
+	}
+
+	copy[count] = (GtCallbackRegistry) { NULL };
+
+	return copy;
+}
+
+static GtCallbackRegistry *
+registry_build(const GtCallbackRegistry *registry, char *instrument_list)
+{
+	int i;
+	GtCallbackRegistry *new = NULL;
+
+	if (NULL == instrument_list) {
+		new = registry_dup(registry);
+	} else if (0 == strlen(instrument_list)) {
+		new = g_new0(GtCallbackRegistry, 1);
+		new[0] = (GtCallbackRegistry) { NULL };
+	} else {
+		int size, j;
+		char *ptr, *token;
+
+		size = 2; /* At least one syscall + NULL record. */
+
+		ptr = strchr(instrument_list, ',');
+		while (NULL != ptr) {
+			size++;
+			ptr = strchr(ptr + 1, ',');
+		}
+
+		new = g_new0(GtCallbackRegistry, size);
+
+		new[size - 1] = (GtCallbackRegistry) { NULL };
+
+		token = strtok_r(instrument_list, ",", &ptr);
+		for (i = 0; i < size - 1; i++) {
+			for (j = 0; NULL != registry[j].name; j++) {
+				if (0 == strcmp(registry[j].name, token)) {
+					new[i] = registry[j];
+					break;
+				}
+			}
+
+			if (NULL == new[j].name) {
+				g_free(new);
+				new = NULL;
+				goto done;
+			}
+
+			token = strtok_r(NULL, ",", &ptr);
+		}
+	}
+
+	if (silent) {
+		for (i = 0; NULL != new[i].name; i++) {
+			new[i].syscall_cb = silent_syscall;
+			new[i].sysret_cb  = silent_sysret;
+		}
+	}
+
+done:
+	return new;
+}
+
 int
 main (int argc, char **argv) {
-	int opt;
+	int opt, count;
 	struct sigaction act;
-	const GtCallbackRegistry *registry;
+	const GtCallbackRegistry *orig_registry;
+	GtCallbackRegistry *registry = NULL;
 	status_t status = VMI_FAILURE;
 
-	while ((opt = getopt(argc, argv, "n:sv")) != -1) {
+	while ((opt = getopt(argc, argv, "hi:n:sv")) != -1) {
 		switch (opt) {
+		case 'i':
+			instrument_list = optarg;
+			break;
 		case 'n':
 			name = optarg;
 			break;
@@ -98,6 +195,7 @@ main (int argc, char **argv) {
 		case 'v':
 			verbose = TRUE;
 			break;
+		case 'h':
 		default:
 			usage();
 			goto done;
@@ -105,6 +203,11 @@ main (int argc, char **argv) {
 	}
 
 	if (NULL == name) {
+		usage();
+		goto done;
+	}
+
+	if (NULL != instrument_list && 0 == strlen(instrument_list)) {
 		usage();
 		goto done;
 	}
@@ -127,17 +230,13 @@ main (int argc, char **argv) {
 	message("identifying OS type ... ");
 
 	GtOSType os = gt_loop_get_ostype(loop);
-	if (silent && os != GT_OS_LINUX) {
-		fprintf(stderr, "only Linux supports silent mode\n");
-		goto done;
-	}
 
 	switch (os) {
 	case GT_OS_LINUX:
-		registry = silent ? GT_LINUX_SILENT_SYSCALLS : GT_LINUX_SYSCALLS;
+		orig_registry = GT_LINUX_SYSCALLS;
 		break;
 	case GT_OS_WINDOWS:
-		registry = GT_WINDOWS_SYSCALLS;
+		orig_registry = GT_WINDOWS_SYSCALLS;
 		break;
 	default:
 		fprintf(stderr, "unknown guest operating system\n");
@@ -145,22 +244,33 @@ main (int argc, char **argv) {
 	}
 
 	message("%s\n", GT_OS_LINUX ? "linux" : "windows");
-	message("establishing callbacks (might take a few seconds)\n");
 
-	if (0 == gt_loop_set_cbs(loop, registry)) {
+	registry = registry_build(orig_registry, instrument_list);
+	if (NULL == registry) {
+		fprintf(stderr, "error building system call registry\n");
+		goto done;
+	}
+
+	message("establishing callbacks (might take a few seconds) ... ");
+
+	count = gt_loop_set_cbs(loop, registry);
+	if (0 == count) {
 		fprintf(stderr, "unable to instrument any system calls\n");
 		goto done;
 	}
 
-	message("starting event loop");
+	message("%d system calls instrumented\n", count);
+
+	message("running event loop ...\n");
 
 	status = VMI_SUCCESS;
 	gt_loop_run(loop);
 
 done:
-	message("freeing event loop");
+	message("freeing event loop\n");
 
 	gt_loop_free(loop);
+	g_free(registry);
 
 	exit(VMI_SUCCESS == status ? EXIT_SUCCESS : EXIT_FAILURE);
 }
