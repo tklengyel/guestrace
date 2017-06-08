e2561bf49c0c7a540c3f26782c1efa5e88c2b98e
W. Michael Petullo <mike@flyn.org>
2017-06-08 13:41:42 -0400

    Prefix static symbols with '_'
    Signed-off-by: W. Michael Petullo <mike@flyn.org>

--- src/functions-windows.c
+++ src/functions-windows.c
@@ -18,7 +18,7 @@ typedef struct offset_definition_t {
 	char *field_name;
 } offset_definition_t;
 
-static offset_definition_t offset_def[] = {
+static offset_definition_t _offset_def[] = {
 	{ GT_OFFSET_WINDOWS_KPCR_PRCB,                                 "_KPCR", "Prcb" },
 	{ GT_OFFSET_WINDOWS_KPRCB_CURRENTTHREAD,                       "_KPRCB",  "CurrentThread" },
 	{ GT_OFFSET_WINDOWS_KTHREAD_PREVIOUSMODE,                      "_KTHREAD",  "PreviousMode" },
@@ -36,8 +36,8 @@ static offset_definition_t offset_def[] = {
 	{ GT_OFFSET_WINDOWS_BAD, NULL, NULL }
 };
 
-static addr_t   offset[GT_OFFSET_WINDOWS_BAD];
-static gboolean initialized = FALSE;
+static addr_t   _offset[GT_OFFSET_WINDOWS_BAD];
+static gboolean _initialized = FALSE;
 
 static gboolean
 _gt_windows_initialize(GtLoop *loop)
@@ -45,7 +45,7 @@ _gt_windows_initialize(GtLoop *loop)
 	const char *rekall_profile;
 	gboolean ok;
 
-	g_assert(!initialized);
+	g_assert(!_initialized);
 
 	rekall_profile = vmi_get_rekall_path(loop->vmi);
 	if (NULL == rekall_profile) {
@@ -54,18 +54,18 @@ _gt_windows_initialize(GtLoop *loop)
 
 	for (int i = 0; i < GT_OFFSET_WINDOWS_BAD; i++) {
 		ok = gt_rekall_symbol_to_rva(rekall_profile,
-		                             offset_def[i].struct_name,
-		                             offset_def[i].field_name,
-		                            &offset[i]);
+		                            _offset_def[i].struct_name,
+		                            _offset_def[i].field_name,
+		                           &_offset[i]);
 		if (!ok) {
 			goto done;
 		}
 	}
 
-	initialized = TRUE;
+	_initialized = TRUE;
 
 done:
-	return initialized;
+	return _initialized;
 }
 
 static privilege_mode_t
@@ -76,7 +76,7 @@ _gt_windows_get_privilege_mode(vmi_instance_t vmi, vmi_event_t *event, gboolean 
 	status_t status;
 	addr_t thread;
 
-	g_assert(initialized);
+	g_assert(_initialized);
 
 	/*
 	 * Testing indicated pidcache flush was necessary to get vaddr
@@ -91,8 +91,8 @@ _gt_windows_get_privilege_mode(vmi_instance_t vmi, vmi_event_t *event, gboolean 
 
 	status = vmi_read_addr_va(loop->vmi,
 	                          event->x86_regs->gs_base
-	                        + offset[GT_OFFSET_WINDOWS_KPCR_PRCB]
-	                        + offset[GT_OFFSET_WINDOWS_KPRCB_CURRENTTHREAD],
+	                       + _offset[GT_OFFSET_WINDOWS_KPCR_PRCB]
+	                       + _offset[GT_OFFSET_WINDOWS_KPRCB_CURRENTTHREAD],
 	                          0,
 	                         &thread);
 	if (VMI_SUCCESS != status) {
@@ -100,7 +100,7 @@ _gt_windows_get_privilege_mode(vmi_instance_t vmi, vmi_event_t *event, gboolean 
 	}
 
 	status = vmi_read_8_va(loop->vmi,
-	                       thread + offset[GT_OFFSET_WINDOWS_KTHREAD_PREVIOUSMODE],
+	                       thread + _offset[GT_OFFSET_WINDOWS_KTHREAD_PREVIOUSMODE],
 	                       0,
 	                      &previous_mode);
 	if (VMI_SUCCESS != status) {
@@ -117,7 +117,7 @@ _gt_windows_cr3_cb(vmi_instance_t vmi, vmi_event_t *event)
 	GtLoop *loop = event->data;
 	uint8_t previous_mode;
 
-	g_assert(initialized);
+	g_assert(_initialized);
 
 	previous_mode = _gt_windows_get_privilege_mode(vmi, event, TRUE);
 	if (USER_MODE == previous_mode) {
@@ -133,7 +133,7 @@ _gt_windows_wait_for_first_process(GtLoop *loop)
 {
 	status_t status = VMI_FAILURE;
 
-	g_assert(initialized);
+	g_assert(_initialized);
 
 	SETUP_REG_EVENT(&loop->cr3_event, CR3, VMI_REGACCESS_W, 0, _gt_windows_cr3_cb);
 
@@ -167,12 +167,12 @@ _windows_get_pid(vmi_instance_t vmi, vmi_event_t *event)
 	addr_t thread, process;
 	gt_pid_t pid = 0;
 
-	g_assert(initialized);
+	g_assert(_initialized);
 
 	status = vmi_read_addr_va(vmi,
 	                          event->x86_regs->gs_base
-	                        + offset[GT_OFFSET_WINDOWS_KPCR_PRCB]
-	                        + offset[GT_OFFSET_WINDOWS_KPRCB_CURRENTTHREAD],
+	                       + _offset[GT_OFFSET_WINDOWS_KPCR_PRCB]
+	                       + _offset[GT_OFFSET_WINDOWS_KPRCB_CURRENTTHREAD],
 	                          0,
 	                         &thread);
 	if (VMI_SUCCESS != status) {
@@ -181,7 +181,7 @@ _windows_get_pid(vmi_instance_t vmi, vmi_event_t *event)
 
 	status = vmi_read_addr_va(vmi,
 	                          thread
-	                        + offset[GT_OFFSET_WINDOWS_KTHREAD_PROCESS],
+	                       + _offset[GT_OFFSET_WINDOWS_KTHREAD_PROCESS],
 	                          0,
 	                         &process);
 	if (VMI_SUCCESS != status) {
@@ -190,7 +190,7 @@ _windows_get_pid(vmi_instance_t vmi, vmi_event_t *event)
 
 	status = vmi_read_32_va(vmi,
 	                        process
-	                      + offset[GT_OFFSET_WINDOWS_EPROCESS_UNIQUEPROCESSID],
+	                     + _offset[GT_OFFSET_WINDOWS_EPROCESS_UNIQUEPROCESSID],
 	                        0,
 	                       &pid);
 	if (VMI_SUCCESS != status) {
@@ -209,12 +209,12 @@ _windows_get_tid(vmi_instance_t vmi, vmi_event_t *event)
 	addr_t thread;
 	gt_tid_t tid = 0;
 
-	g_assert(initialized);
+	g_assert(_initialized);
 
 	status = vmi_read_addr_va(vmi,
 	                          event->x86_regs->gs_base
-	                        + offset[GT_OFFSET_WINDOWS_KPCR_PRCB]
-	                        + offset[GT_OFFSET_WINDOWS_KPRCB_CURRENTTHREAD],
+	                       + _offset[GT_OFFSET_WINDOWS_KPCR_PRCB]
+	                       + _offset[GT_OFFSET_WINDOWS_KPRCB_CURRENTTHREAD],
 	                          0,
 	                         &thread);
 	if (VMI_SUCCESS != status) {
@@ -223,8 +223,8 @@ _windows_get_tid(vmi_instance_t vmi, vmi_event_t *event)
 
 	status = vmi_read_addr_va(vmi,
 	                        thread
-	                      + offset[GT_OFFSET_WINDOWS_ETHREAD_CID]
-	                      + offset[GT_OFFSET_WINDOWS_CLIENT_ID_UNIQUETHREAD],
+	                     + _offset[GT_OFFSET_WINDOWS_ETHREAD_CID]
+	                     + _offset[GT_OFFSET_WINDOWS_CLIENT_ID_UNIQUETHREAD],
 	                        0,
 	                       &tid);
 	if (VMI_SUCCESS != status) {
@@ -244,12 +244,12 @@ _gt_windows_get_process_name(vmi_instance_t vmi, vmi_event_t *event)
 	addr_t thread, process;
 	char *proc = NULL;
 
-	g_assert(initialized);
+	g_assert(_initialized);
 
 	status = vmi_read_addr_va(vmi,
 	                          event->x86_regs->gs_base
-	                        + offset[GT_OFFSET_WINDOWS_KPCR_PRCB]
-	                        + offset[GT_OFFSET_WINDOWS_KPRCB_CURRENTTHREAD],
+	                       + _offset[GT_OFFSET_WINDOWS_KPCR_PRCB]
+	                       + _offset[GT_OFFSET_WINDOWS_KPRCB_CURRENTTHREAD],
 	                          0,
 	                         &thread);
 	if (VMI_SUCCESS != status) {
@@ -258,7 +258,7 @@ _gt_windows_get_process_name(vmi_instance_t vmi, vmi_event_t *event)
 
 	status = vmi_read_addr_va(vmi,
 	                          thread
-	                        + offset[GT_OFFSET_WINDOWS_KTHREAD_PROCESS],
+	                       + _offset[GT_OFFSET_WINDOWS_KTHREAD_PROCESS],
 	                          0,
 	                         &process);
 	if (VMI_SUCCESS != status) {
@@ -267,7 +267,7 @@ _gt_windows_get_process_name(vmi_instance_t vmi, vmi_event_t *event)
 
 	proc = vmi_read_str_va(vmi,
 	                       process
-	                     + offset[GT_OFFSET_WINDOWS_EPROCESS_PNAME],
+	                    + _offset[GT_OFFSET_WINDOWS_EPROCESS_PNAME],
 	                       0);
 	if (NULL == proc) {
 		goto done;
@@ -290,7 +290,7 @@ _gt_windows_is_user_call(GtLoop *loop, vmi_event_t *event)
 	gboolean ok;
 	uint8_t previous_mode;
 
-	g_assert(initialized);
+	g_assert(_initialized);
 
 	previous_mode = _gt_windows_get_privilege_mode(loop->vmi, event, FALSE);
 	if (USER_MODE == previous_mode) {
@@ -305,7 +305,7 @@ _gt_windows_is_user_call(GtLoop *loop, vmi_event_t *event)
 static addr_t
 _gt_windows_get_offset(int offset_id)
 {
-	return offset_id > GT_OFFSET_WINDOWS_BAD ? 0 : offset[offset_id];
+	return offset_id > GT_OFFSET_WINDOWS_BAD ? 0 : _offset[offset_id];
 }
 
 struct os_functions os_functions_windows = {
